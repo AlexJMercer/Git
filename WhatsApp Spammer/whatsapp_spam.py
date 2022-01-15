d11385597ad02cef0579e8aa744f4821284da46c
Tathagata Guha Ray <nguharay71@gmail.com>
2022-01-15 22:01:17 +0530

    replaced while loop with for

--- WhatsApp Spammer/whatsapp_spam.py
+++ WhatsApp Spammer/whatsapp_spam.py
@@ -6,7 +6,6 @@ num = int(input("Enter number of messages :  "))
 text = input("Enter text : ")
 time.sleep(5)
 
-while i<=num:
+for i in range(num):
     pyautogui.typewrite(text)
     pyautogui.press('enter')
-    i += 1
